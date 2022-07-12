3a37087c585a113983088ee4193a8df1c6a6404e
Álvaro Romário Cavalcante Alencar <alvaroromario1@hotmail.com>
2022-07-12 12:46:27 -0300

    feat: adding userLogin method request to Api.controller.js

--- src/js/controllers/Api.controller.js
+++ src/js/controllers/Api.controller.js
@@ -2,8 +2,37 @@
 
 export class Api {
 
-    static async newApiRequest() {
-        // example
+    static baseUrl = "https://habits-kenzie.herokuapp.com"
+    static token = JSON.parse(localStorage.getItem("@Kenzie-Habit-M2:token"))
+    static headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.token}`
+    }
+
+    static async userLogin(loginData) {
+
+        const loginUrl = this.baseUrl + "/api/userLogin"
+
+        return await fetch(loginUrl, {
+            method: "POST",
+            headers: this.headers,
+            body: JSON.stringify(loginData)
+        })
+            .then(res => res.json())
+            .then(res => {
+                if (res.token && res.response) {
+                    localStorage.setItem("@Kenzie-Habit-M2:token", JSON.stringify(res.token))
+                    localStorage.setItem("@Kenzie-Habit-M2:user", JSON.stringify(res.response))
+                }
+                if (res.message) {
+                    console.log(res.message)
+                }
+                return res
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
     }
 
 }
